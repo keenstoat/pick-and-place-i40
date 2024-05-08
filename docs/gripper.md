f8ee17fbdd468f6934b7a0a39227134ca5d0e354
charles <>
2024-05-08 11:13:04 +0200

    gripper firmware complete

--- docs/gripper.md
+++ docs/gripper.md
@@ -12,3 +12,24 @@ There is a way to control these two servos using the RberryPi:
 - [Using PWM directly](https://projects.raspberrypi.org/en/projects/grandpa-scarer/3). 
 - [RaspberryPi python package](https://www.digikey.com/en/maker/tutorials/2021/how-to-control-servo-motors-with-a-raspberry-pi)
 
+
+# Arduino
+
+The mc code reads a string from the Serial interface and parses it as JSON. 
+
+
+
+```
+{"action": "status"}
+{"action": "open", "value": 0, "relative": true}
+{"action": "rotate", "value": 0, "relative": true}
+```
+
+
+# Refs
+
+https://randomnerdtutorials.com/esp32-servo-motor-web-server-arduino-ide/
+
+https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
+
+https://arduino.stackexchange.com/questions/84328/getting-all-data-of-my-json-object-from-serial-read-at-once
--- gripper/gripper.ino
+++ gripper/gripper.ino
@@ -1,47 +1,41 @@
 #include <ArduinoJson.h>
 #include <Servo.h>
-//#include <ESP32Servo.h>
-
 
 #define GRIP_SERVO_PIN 26
 #define ROTATION_SERVO_PIN 27
 
-#define GRIP_MIN_ANGLE 60
-#define GRIP_MAX_ANGLE 165
+#define GRIP_MIN_ANGLE 0
+#define GRIP_MAX_ANGLE 100
 
-#define ROTATE_MIN_ANGLE 48
-#define ROTATE_MAX_ANGLE 134
+#define ROTATE_MIN_ANGLE 3
+#define ROTATE_MAX_ANGLE 135
 
 Servo servoGrip;
 Servo servoRotate;
 
+int opening = 0;
+int rotation = 0;
+
 JsonDocument jsonDoc;
 
-
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-    // servoGrip.attach(GRIP_SERVO_PIN, Servo::CHANNEL_NOT_ATTACHED, GRIP_MIN_ANGLE, GRIP_MAX_ANGLE);
-    // servoRotate.attach(ROTATION_SERVO_PIN, Servo::CHANNEL_NOT_ATTACHED, ROTATE_MIN_ANGLE, ROTATE_MAX_ANGLE);
     servoGrip.attach(GRIP_SERVO_PIN);
     servoRotate.attach(ROTATION_SERVO_PIN);
-    servoGrip.write(0);
-    servoRotate.write(0);
 
+    // initialize griper openning and rotation 
+    servoGrip.write(GRIP_MIN_ANGLE);
+    servoRotate.write(ROTATE_MIN_ANGLE);
 }
 
-void loop() {
-
-    if(Serial.available() > 0) {
-
-        /**
-        {"action": "status"}
-        {"action": "grip", "value": 0}
-        {"action": "rotate", "value": 0}
-        */
+void loop()
+{
+    if (Serial.available() > 0) {
 
         const auto error = deserializeJson(jsonDoc, Serial);
 
-        if(error) {
+        if (error) {
             Serial.print(F("{\"error\": \""));
             Serial.print(error.c_str());
             Serial.println("\"}");
@@ -49,48 +43,76 @@ void loop() {
         } else {
             String action = jsonDoc["action"];
 
-            if(action == "status") {
+            if (action == "status") {
                 sendStatus();
 
-            } else if(action == "grip") {
-                int gripAngle = jsonDoc["value"];
-                actuateGrip(gripAngle);
+            } else if (action == "open") {
+                openGripper();
 
-            } else if(action == "rotate") {
-                int rotateAngle = jsonDoc["value"];
-                rotate(rotateAngle);
-                
+            } else if (action == "rotate") {
+                rotateGripper();
+
             } else {
-
-                Serial.println("no option");
-                Serial.println(action);
+                sendError("unknown action '"+ action +"'");
             }
-            
         }
     }
 }
 
+void sendError(String msg){
+    Serial.print(F("{\"error\": \""));
+    Serial.print(msg);
+    Serial.println("\"}");
+}
+
 void sendStatus() {
     JsonDocument doc;
-    deserializeJson(doc, "{\"ok\": true}");
+    deserializeJson(doc, "{}");
+    doc["opening"] = opening;
+    doc["rotation"] = rotation;
     serializeJson(doc, Serial);
     Serial.println();
 }
 
-void actuateGrip(int value) {
-    if(value < 0 || value > 100)
+void openGripper() {
+
+    int value = jsonDoc["value"];
+    bool isRelative = jsonDoc["relative"] ? jsonDoc["relative"] : false;
+
+    if(!value) {
+        sendError("must specify a value to open gripper");
         return;
-    value = GRIP_MIN_ANGLE + value;
+    }
+        
+    value = isRelative ? opening + value : value;
+
+    if (value < 0 || value > 100) {
+        sendError("the gripper can only open between 0 and 100 percent");
+        return;
+    }
+
+    opening = value;
     servoGrip.write(value);
-    Serial.println(value);
 }
 
-void rotate(int value) {
+void rotateGripper() {
 
-    if(value < 0 || value > 180)
+    int value = jsonDoc["value"];
+    bool isRelative = jsonDoc["relative"] ? jsonDoc["relative"] : false;
+
+    if(!value) {
+        sendError("must specify a value to rotate gripper");
         return;
+    }
 
-//    value = ROTATE_MIN_ANGLE + value;
+    value = isRelative ? rotation + value : value;
+
+    if (value < 0 || value > 180) {
+        sendError("the gripper can only rotate between 0 and 180 degrees");
+        return;
+    }
+
+    rotation = value;
+    value = ROTATE_MIN_ANGLE + (value * (ROTATE_MAX_ANGLE - ROTATE_MIN_ANGLE) / 180);
     servoRotate.write(value);
-    Serial.println(value);
 }
