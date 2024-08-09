1cee7c0462b6190bebabe6c0cc3bc67d951e0b47
charles <charles@hsel.com>
2024-08-09 12:12:24 +0200

    changes

--- integration_api/rest_api/api_functions.py
+++ integration_api/rest_api/api_functions.py
@@ -8,12 +8,12 @@ ROBOT_BASE_TO_END_EFFECTOR_BASE_INITIAL_DISTANCE = 290 # mm
 ROBOT_Z_RANGE = 300 # mm
 GRIPPER_HEIGHT = 140 # mm
 
-TABLE_DISTANCE_FROM_ROBOT_BASE = None
+_DELTA_ROBOT_IP_ADDRESS = "192.168.3.11"
+_DELTA_ROBOT_PORT = 502
 
-
-DELTA_ROBOT_IP_ADDRESS = "192.168.3.11"
-DELTA_ROBOT_PORT = 502
-
+_robot_ip_address = None
+_robot_port = None
+_table_distance_from_robot_base = None
 
 def status():
     response = {
@@ -23,19 +23,19 @@ def status():
 
 def is_module_initialized():
 
-    robot = DeltaRobot(DELTA_ROBOT_IP_ADDRESS, port=DELTA_ROBOT_PORT)
+    robot = get_robot()
     response = {
         "data": robot.is_enabled() and robot.is_referenced()
     }
     return json.dumps(response), 200
 
 def init_module():
+        global _robot_ip_address, _robot_port
         
-        def init(robot_ip_address, robot_port):
+        def init():
             gripper = Gripper()
-            robot = DeltaRobot(robot_ip_address, port=robot_port)
-            assert robot.is_connected, "DeltaRobot is not connected"
-
+            robot = get_robot()
+            
             print("Has General Error       : ", robot.has_module_error())
             print("Has Kinemat Error       : ", robot.has_kinematics_error())
             print()
@@ -66,9 +66,9 @@ def init_module():
             gripper.rotate(90)
 
         data = request.json["data"]
-        robot_ip_address = data.get("robotIpAddress", DELTA_ROBOT_IP_ADDRESS)
-        robot_port = int(data.get("robotPort", DELTA_ROBOT_PORT))
-        threading.Thread(target=init, args=[robot_ip_address, robot_port]).start()
+        _robot_ip_address = data.get("robotIpAddress", _DELTA_ROBOT_IP_ADDRESS)
+        _robot_port = int(data.get("robotPort", _DELTA_ROBOT_PORT))
+        threading.Thread(target=init, args=[_robot_ip_address, _robot_port]).start()
         response = {
             "data": ""
         }
@@ -76,10 +76,10 @@ def init_module():
 
 def set_table_distance():
 
-    data = request.json["data"]
+    global _table_distance_from_robot_base
 
-    global TABLE_DISTANCE_FROM_ROBOT_BASE
-    TABLE_DISTANCE_FROM_ROBOT_BASE = float(data)
+    data = request.json["data"]
+    _table_distance_from_robot_base = float(data)
 
     response = {
          "data": ""
@@ -90,7 +90,7 @@ def get_robot_position_xyz():
 
     coord = request.url.split("/")[-1]
 
-    robot = DeltaRobot(DELTA_ROBOT_IP_ADDRESS, port=DELTA_ROBOT_PORT)
+    robot = get_robot()
     x, y, z = robot.get_target_position_cart()
 
     data = {"x": x, "y": y, "z": z}
@@ -105,7 +105,7 @@ def get_robot_position_xyz():
 
 def get_set_robot_speed():
 
-    robot = DeltaRobot(DELTA_ROBOT_IP_ADDRESS, port=DELTA_ROBOT_PORT)
+    robot = get_robot()
 
     if request.method == "GET":
         response = {
@@ -117,6 +117,14 @@ def get_set_robot_speed():
     robot.set_speed(speed)
     return '', 201
     
+def is_module_busy():
+
+    robot = get_robot()
+    response = {
+        "data": robot.is_moving()
+    }
+    return json.dumps(response), 200
+
 def move_robot():
  
     xyzs:dict = request.json["data"]
@@ -127,22 +135,62 @@ def move_robot():
     if xyzs["speed"]:
         xyzs["speed"] = int(xyzs["speed"])
 
-    robot = DeltaRobot(DELTA_ROBOT_IP_ADDRESS, port=DELTA_ROBOT_PORT)
+    robot = get_robot()
     threading.Thread(target=robot.move_cartesian, args=[xyzs["x"], xyzs["y"], xyzs["z"], xyzs["speed"]]).start()
     response = {
         "data": ""
     }
     return json.dumps(response), 202
 
-def is_module_busy():
+def pick_and_place(x_ini, y_ini, x_end, y_end, object_width, object_height):
+    robot = get_robot()
+    gripper = Gripper()
+    robot_lowest_position = get_robot_lowest_position()
+    z_ini = robot_lowest_position + object_height + 10 # mm
 
-    robot = DeltaRobot(DELTA_ROBOT_IP_ADDRESS, port=DELTA_ROBOT_PORT)
-    response = {
-        "data": robot.is_moving()
-    }
-    return json.dumps(response), 200
+    # Pick Object =======================================================
+    # step("position above object")
+    robot.move_cartesian(x=x_ini, y=y_ini, z=z_ini)
+    
+    # step("open gripper...")
+    gripper.open(object_width + 10)
+
+    # step("position to grab object")
+    robot.move_cartesian(z=robot_lowest_position)
+    
+    # step("close gripper...")
+    gripper.open(object_width-5)
+
+    # step("pick up object")
+    robot.move_cartesian(x=0, y=0, z=ROBOT_Z_RANGE)
+
+    # Place Object =======================================================
+    # step("position to place object")
+    robot.move_cartesian(x=x_end, y=y_end, z=robot_lowest_position)
+    
+    # step("open gripper...")
+    gripper.open_mm(object_width + 10)
+
+    # step("position above object")
+    robot.move_cartesian(z=z_ini)
+
+    # step("return to base position")
+    robot.move_cartesian(x=0, y=0, z=ROBOT_Z_RANGE)
+    
+    # step("close gripper...")
+    gripper.open(0)
+
 
 # aux functions ========================================================================================================
 
 def get_robot_lowest_position():
-    z = ROBOT_Z_RANGE - (TABLE_DISTANCE_FROM_ROBOT_BASE - ROBOT_BASE_TO_END_EFFECTOR_BASE_INITIAL_DISTANCE) + GRIPPER_HEIGHT
+    z = ROBOT_Z_RANGE - (_table_distance_from_robot_base - ROBOT_BASE_TO_END_EFFECTOR_BASE_INITIAL_DISTANCE) + GRIPPER_HEIGHT
+    return z
+
+def get_robot():
+    
+    assert _robot_ip_address and _robot_port, "Robot not initialized. IP Address and port not set."
+    robot = DeltaRobot(_robot_ip_address, port=_robot_port)
+    assert robot.is_connected, f"DeltaRobot could not connect to {_robot_ip_address} on port {_robot_port}"
+    return robot
+    
